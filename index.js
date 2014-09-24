a28517796ff3433142bc3f6ce2b4fac2e597d5e9
hefangshi <hefangshi@gmail.com>
2014-09-24 19:16:50 +0800

    add default path

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ var cwd = process.cwd();
 var bcs = require('./lib/bcsClient.js')
 
 module.exports = function (dest, file, settings, callback) {
+    var to = (dest.to || '/').replace(/\/*$/, '');
     bcs.upload(
-        settings, file.getContent(), dest.to.replace(/\/*$/, '') + dest.release,
+        settings, file.getContent(), to + dest.release,
         function(err, res){
             if(err || res.trim() != '0'){
-                fis.log.error('upload file [' + file.subpath + '] to [' + dest.to +
+                fis.log.error('upload file [' + file.subpath + '] to [' + to +
                     '] by receiver [bcs] error [' + (err || res) + ']');
             } else {
                 var time = '[' + fis.log.now(true) + ']';
@@ -16,7 +17,7 @@ module.exports = function (dest, file, settings, callback) {
                     time.grey + ' ' + 
                     file.subpath.replace(/^\//, '') +
                     ' >> '.yellow.bold +
-                    dest.to + dest.release +
+                    to + dest.release +
                     '\n'
                 );
                 callback();
